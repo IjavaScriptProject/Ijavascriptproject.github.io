b3ba28c07de00cbdf55085314a0671c5b4389fa0
Istamikhon <66559691+Istamikhon@users.noreply.github.com>
2022-05-27 11:03:32 +0300

    test
    test

---
+++ app.js
@@ -0,0 +1,16 @@
+const API_KEY = '8c8e1a50-6322-4135-8875-5d40a5420d86';
+const API_URL_POPULAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_100_POPULAR_FILMS&page=1';
+const API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=';
+
+getMovies(API_URL_POPULAR);
+
+async function getMovies(url) {
+  const resp = await fetch(url, {
+    headers: {
+      "Content-Type": "application/json",
+      "X-API-KEY": API_KEY,
+    },
+  });
+  const respData = await resp.json();
+  showMovies(respData);
+}
